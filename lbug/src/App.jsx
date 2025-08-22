52a484c7e65a3928374cd32cffeda5ba92cc7186
sera <sera9898@naver.com>
2025-08-22 17:31:57 +0900

    first commit

---
+++ lbug/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+// App.jsx
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>예전처럼 그대로 코딩하면 됩니다 😎</p>
+    </div>
+  );
+}
+
+export default App;
+
+
